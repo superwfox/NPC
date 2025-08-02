824e7881e1efd766333362670858474d9ebeb26f
SUDARK <2963502563@qq.com>
2025-08-02 22:28:50 +0800

    Sudark

--- src/main/java/sudark2/Sudark/nPC/NPC.java
+++ src/main/java/sudark2/Sudark/nPC/NPC.java
@@ -1,47 +1,38 @@
 package sudark2.Sudark.nPC;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Villager;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import static sudark2.Sudark.nPC.NPCManager.villagers;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import static sudark2.Sudark.nPC.NPCManager.showTime;
 
 public final class NPC extends JavaPlugin {
 
     static World main = null;
+    static Set<UUID> displays = new HashSet<>();
 
     @Override
     public void onEnable() {
-        initialise(this);
-    }
-
-    public void initialise(Plugin plugin) {
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                main = Bukkit.getWorld("BEEF-DUNE");
-                main.getEntities().forEach(entity -> {
-                    if(entity.isCustomNameVisible()){
-                        entity.remove();
-                    }
-                });
-
-                NPCManager npcManager = new NPCManager(plugin);
-                npcManager.spawnNPC(new Location(main, 5, 82, 1), "§e农民", Villager.Profession.FARMER);
-                npcManager.spawnNPC(new Location(main, 5, 82, -5), "§e铁匠铺", Villager.Profession.ARMORER);
-                npcManager.spawnNPC(new Location(main, 4, 82, 33), "§e染铺夫人", Villager.Profession.LEATHERWORKER);
-                npcManager.spawnNPC(new Location(main, 4, 82, 39), "§e建材商", Villager.Profession.MASON);
-            }
-        }.runTaskLater(this, 5 * 20L);
+        main = Bukkit.getWorld("BEEF-DUNE");
+        showTime(this);
+        Bukkit.getPluginManager().registerEvents(new NPCManager(), this);
     }
 
     @Override
     public void onDisable() {
-        villagers.forEach(Entity::remove);
+        displays.removeIf(uuid -> {
+            Entity entity = Bukkit.getEntity(uuid);
+            if (entity != null) {
+                entity.remove();
+                return true;
+            }
+            return false;
+        });
+
     }
 }
--- src/main/java/sudark2/Sudark/nPC/NPCManager.java
+++ src/main/java/sudark2/Sudark/nPC/NPCManager.java
@@ -2,73 +2,139 @@ package sudark2.Sudark.nPC;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.entity.Entity;
+import org.bukkit.Material;
+import org.bukkit.block.Banner;
+import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Villager;
+import org.bukkit.entity.ItemDisplay;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityRemoveEvent;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BannerMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Transformation;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+
+import static sudark2.Sudark.nPC.NPC.displays;
+import static sudark2.Sudark.nPC.NPC.main;
 
 public class NPCManager implements Listener {
-    private final Plugin plugin;
-    public static Set<Villager> villagers = new HashSet<>();
+    static Location shop1 = p(124, 66, -1);
+    static Location shop2 = p(124, 66, 41);
+    static Location shop3 = p(158, 66, -1);
+    static Location shop4 = p(158, 66, 41);
 
-    public NPCManager(Plugin plugin) {
-        this.plugin = plugin;
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        startNPCChecker();
-    }
+    static Set<Location> settlements = Set.of(
+            p(107, 63, 9), p(107, 63, 31),
+            p(141, 63, 9), p(141, 63, 31),
+            p(175, 63, 9), p(175, 63, 31)
+    );
 
-    // 创建村民NPC
-    public void spawnNPC(Location location, String name, Villager.Profession profession) {
-        Villager villager = (Villager) location.getWorld().spawnEntity(location.clone().add(0.5, 0, 0.5), EntityType.VILLAGER);
-        villager.setCustomName(name);
-        villager.setCustomNameVisible(true);
-        villager.setProfession(profession);
-        villager.setInvulnerable(true);
-        villager.setAI(false);
-        villager.addScoreboardTag("npc");
+    static Transformation huge = new Transformation(
+            new Vector3f(0, 5f, 0),            // 平移（translation）向量
+            new Quaternionf(),  // 旋转（rotation），默认不旋转
+            new Vector3f(6f, 6f, 6f), // 缩放（scale）到 1
+            new Quaternionf()                // 旋转中心点（leftRotation），默认不旋转
+    );
 
-        // 存储NPC信息
-        villagers.add(villager);
-    }
+    static Transformation drama = new Transformation(
+            new Vector3f(0, 7f, 0),            // 平移（translation）向量
+            new Quaternionf(),  // 旋转（rotation），默认不旋转
+            new Vector3f(8f, 7f, 8f), // 缩放（scale）到 1
+            new Quaternionf()                // 旋转中心点（leftRotation），默认不旋转
+    );
 
-    // 定时检查NPC状态
-    private void startNPCChecker() {
+    public static void showTime(Plugin plugin) {
+        reGenerate();
         new BukkitRunnable() {
             @Override
             public void run() {
-                if (villagers.isEmpty()) return;
-                for (Villager entity : villagers) {
-                    if (entity != null) {
-                        entity.getNearbyEntities(10, 10, 10).stream()
-                                .filter(e -> e instanceof org.bukkit.entity.Player)
-                                .findFirst()
-                                .ifPresent(player -> {
-                                    Location playerLoc = player.getLocation();
-                                    Location entityLoc = entity.getLocation();
-                                    entityLoc.setDirection(playerLoc.toVector().subtract(entityLoc.toVector()));
-                                    entity.teleport(entityLoc);
-                                });
-                    }
-                }
+
+                displays.forEach(display -> {
+                    rotateDisplay((ItemDisplay) Bukkit.getEntity(display), 1.2f);
+                });
+
             }
-        }.runTaskTimer(plugin, 0L, 10L); // 每0.5秒检查一次
+        }.runTaskTimer(plugin, 0, 20);
     }
 
-    // 防止NPC被破坏
     @EventHandler
-    public void onEntityDamage(EntityDamageEvent event) {
-        Entity entity = event.getEntity();
-        if (entity instanceof Villager v) {
-            if (entity.getScoreboardTags().contains("npc")) {
-                event.setCancelled(true);
+    public void onDisplayDead(PlayerChatEvent event) {
+        Player player = event.getPlayer();
+        if (player.isOp() && event.getMessage().equals("regenerate")) {
+            reGenerate();
+            Bukkit.broadcastMessage("NPCs have been regenerated!");
+        }
+    }
+
+    public static void reGenerate() {
+        main.getEntities().forEach(
+                entity -> {
+                    if (entity instanceof ItemDisplay)
+                        entity.remove();
+                });
+        createShop(shop1, Material.STONECUTTER);
+        createShop(shop2, Material.ORANGE_GLAZED_TERRACOTTA);
+        createShop(shop3, Material.BARREL);
+        createShop(shop4, Material.BLAST_FURNACE);
+        for (Location settlement : settlements) {
+            if (getBanner(settlement) != null) {
+                ItemDisplay display = (ItemDisplay) settlement.getWorld().spawnEntity(settlement, EntityType.ITEM_DISPLAY);
+                display.setItemStack(getBanner(settlement));
+                display.setTransformation(drama);
+                display.setInterpolationDelay(0);
+                displays.add(display.getUniqueId());
             }
         }
+    }
+
+    public static void createShop(Location loc, Material m) {
+        ItemDisplay item = (ItemDisplay) loc.getWorld().spawnEntity(loc, EntityType.ITEM_DISPLAY);
+        item.setItemStack(new ItemStack(m));
+        item.setTransformation(huge);
+        item.setInterpolationDelay(0);
+        displays.add(item.getUniqueId());
+    }
+
+    public static void rotateDisplay(ItemDisplay display, float radiansPerSecond) {
+        if (display == null) return;
+        Transformation tf = display.getTransformation();
+        tf.getLeftRotation()
+                .rotateLocalY(radiansPerSecond / 20f);  // 每tick 增加	(弧度/秒 ÷ 20)
+
+        display.setInterpolationDuration(20);
+        display.setInterpolationDelay(0);
+        display.setTransformation(tf);
+
+    }
+
+
+    public static Location p(int x, int y, int z) {
+        return new Location(main, x, y, z);
+    }
+
+    public static ItemStack getBanner(Location loc) {
+        Block block = loc.getBlock();
+
+        if (block.getType().toString().endsWith("_BANNER")) {
+            Banner banner = (Banner) block.getState();
+
+            ItemStack item = new ItemStack(block.getType());
+            BannerMeta meta = (BannerMeta) item.getItemMeta();
+
+            meta.setPatterns(banner.getPatterns());
+            item.setItemMeta(meta);
+
+            return item;
+        }
+        return null;
     }
 }
